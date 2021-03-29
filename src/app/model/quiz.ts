f7649e0a23147559b31af5c82ef74437614399fd
bobbyfromgeminus <info.geminus@gmail.com>
2021-03-29 14:45:19 +0200

    Classes, components

--- src/app/model/quiz.ts
+++ src/app/model/quiz.ts
@@ -1,2 +1,9 @@
+import { Question } from "./question";
+
 export class Quiz {
+    id: number = 0;
+    title: string = '';
+    description: string = '';
+    questions: Question = new Question();
+    active: boolean = true;
 }
