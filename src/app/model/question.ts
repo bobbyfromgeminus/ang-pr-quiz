f27c4d74ddbe132e369e6f105276ae17d7b5db91
bobbyfromgeminus <info.geminus@gmail.com>
2021-03-29 16:34:32 +0200

    Classes, components

--- src/app/model/question.ts
+++ src/app/model/question.ts
@@ -3,7 +3,7 @@ import { Answer } from "./answer";
 export class Question {
     id: number = 0;
     question: string = '';
-    answers: Answer = new Answer;
+    answers: Answer[] = new Array;
     points: number = 0;
     active: boolean = true;
 }
